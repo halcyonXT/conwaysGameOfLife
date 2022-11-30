4634ea5b3855306d34ccdb25143c306b6ec5fc25
Halcyon <111774390+halcyonXT@users.noreply.github.com>
2022-11-30 12:59:48 +0100

    Major update

--- main.js
+++ main.js
@@ -10,7 +10,17 @@ class Cell {
 
 let cells = []
 let idBar = 1
-let SIZE = 48
+let SIZE = 4
+let speed = 250
+let timer
+let resizedFlag = 0
+let userDefColor = "#FFFFFF"
+let timesPressed = 5;
+let saveStateLimit = 20
+let savedStates = []
+for (let i=0; i < 20; i++) {
+    savedStates.push([null])
+}
 
 const isQueued = (id) => {
     id--
@@ -34,19 +44,21 @@ const isQueued = (id) => {
     }
 }
 
-function generate() {
+const generate = () => {
     let mainframe = document.getElementById("mainframe");
+    mainframe.innerHTML = ``;
     let exclusionary;
-    for (let i=1; i < 49; i++) {
+    let side = 480 / SIZE
+    for (let i=1; i < SIZE+1; i++) {
         mainframe.innerHTML += `<tr id="row${i}">`;
 
-        for (let j=1; j < 49; j++) {
+        for (let j=1; j < SIZE+1; j++) {
             if (j == 1 || j == SIZE) {
                 switch (j) {
                     case 1:
                         exclusionary = "left"
                         break;
-                    case SIZE:
+                    case SIZE-resizedFlag:
                         exclusionary = "right"
                         break;
                 }
@@ -54,10 +66,18 @@ function generate() {
 
             cells.push(new Cell(idBar, false, 0, exclusionary))
             document.getElementById(`row${i}`).innerHTML += 
-            `<td class="cells" id="cell${idBar}" onmouseup="convert(${idBar})"></td>`
+            `<td class="cells" id="cell${idBar}" onmouseup="convert(${idBar})" 
+            style="height:${side}px;width:${side}px"></td>`
             idBar++;
+            console.log(`idBar:${idBar}\nSize:${SIZE}\nside:${side}\ni:${i}\nj:${j}`)
+            if (j == SIZE) {
+                break;
+            }
         }
         mainframe.innerHTML += `</tr>`
+        if (i == SIZE) {
+            break;
+        }
     }
 }
 
@@ -116,7 +136,7 @@ const convert = (id) => {
 
         break;
         case false:
-            target.style.backgroundColor = "white";
+            target.style.backgroundColor = userDefColor;
             cells[targetId].isAlive = true;
             switch(cells[targetId].borderExc) {
                 case "right":
@@ -160,18 +180,18 @@ const convert = (id) => {
     }
     //console.log(cells[targetId])
 }
-
+//<button id="begin" onclick="begin()">BEGIN</button>
 const begin = () => {
     isActive = !isActive
     switch (isActive) {
         case true:
-            document.getElementById("begin").innerText = "PAUSE"
-            document.getElementById("mainframe").style.pointerEvents = "none";
+            document.getElementById("play").src = "./resources/pause.png"
+            //document.getElementById("mainframe").style.pointerEvents = "none";
             buildQueue()
         break;
         case false:
-            document.getElementById("begin").innerText = "BEGIN"
-            document.getElementById("mainframe").style.pointerEvents = "all";
+            document.getElementById("play").src = "./resources/play.png"
+            //document.getElementById("mainframe").style.pointerEvents = "all";
         break;
     }
 }
@@ -192,6 +212,17 @@ const buildQueue = () => {
     }
 }
 
+const saveState = () => {
+    let temp = []
+    for (let cell of cells) {
+        if (cell.isAlive) {
+            temp.push(cell.id)
+        }
+    }
+    savedStates.push(temp)
+    savedStates.shift()
+}
+
 const executeQueue = () => {
     queuedInversions.forEach(item => {
         convert(item)
@@ -199,14 +230,68 @@ const executeQueue = () => {
 }
 
 const gameflow = () => {
-    setInterval(() => {
+    timer = setInterval(() => {
         if (isActive) {
+            timesPressed = saveStateLimit;
+            saveState();
             buildQueue();
             //console.log("BUILD:" + queuedInversions)
             executeQueue();
             //console.log("EXEC:" + queuedInversions)
         }
-    },250)
+    },speed)
+}
+
+const forward = () => {
+    timesPressed = saveStateLimit
+    saveState()
+    buildQueue()
+    executeQueue()
+}
+
+document.getElementById("speed").addEventListener("change", () => {
+    speed = 1525 - document.getElementById("speed").value;
+    clearInterval(timer)
+    timer = null;
+    gameflow()
+})
+
+
+const flush = () => {
+    cells = []
+    idBar = 1;
+    queuedInversions = []
+    isActive = false
+    savedStates = []
+    SIZE = Number(document.getElementById("size").value);
+    document.getElementById("mainframe").innerHTML = ``;
+    generate()
+}
+
+const backward = () => {
+    if (timesPressed != 0) {
+        timesPressed--
+    }
+    for (let cell of cells) {
+        if (cell.isAlive) {
+            convert(cell.id)
+        }
+    }
+    for (let cell of savedStates[timesPressed]) {
+        convert(cell)
+    }
+}
+
+const updateSavestate = () => {
+    savedStates = []
+    saveStateLimit = Number(document.getElementById("ss--input").value)
+    if (saveStateLimit > 100) {
+        saveStateLimit = 100
+        document.getElementById("ss--input").value = "100"
+    }
+    for (let i=0; i < saveStateLimit; i++) {
+        savedStates.push([null])
+    }
 }
 
 gameflow()
